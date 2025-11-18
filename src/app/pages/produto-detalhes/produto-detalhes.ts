c04bb44f9c1cf982fa3155227440b7b4dcad2c83
Paulo Victor <paulovictormudex@gmail.com>
2025-11-18 20:43:45 -0300

    Primeiro commit - Casinha dos Doces

---
+++ src/app/pages/produto-detalhes/produto-detalhes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-detalhes',
+  imports: [],
+  templateUrl: './produto-detalhes.html',
+  styleUrl: './produto-detalhes.css',
+})
+export class ProdutoDetalhes {
+
+}
